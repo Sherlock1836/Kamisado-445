b65cd06aff1497b064c1460178a852762a0cbca9
Isaac Reynolds <73602312+Sherlock1836@users.noreply.github.com>
2024-03-19 15:44:38 -0500

    added another check to move validator

--- src/MoveValidator.java
+++ src/MoveValidator.java
@@ -2,6 +2,8 @@ public class MoveValidator {
     public boolean checkValidityOf(Square[] move) {
         if(move[1].equals(move[0]))
             return false;
+        if(move[1].getDragonTower() != null)
+            return false;
         return true;
     }
 }
